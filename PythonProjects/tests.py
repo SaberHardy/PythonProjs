06054cf8ad389bc42959846a2cc283eae8910b29
Consultant <samirsamado5@gmail.com>
2023-09-05 16:41:38 -0400

    collections

--- PythonProjects/tests.py
+++ PythonProjects/tests.py
@@ -10,6 +10,20 @@
 # else:
 #     print('More')
 
-words = ['cat', 'window', 'defenestrate']
-for w in words:
-    print(w, len(w))
+# words = ['cat', 'window', 'defenestrate']
+# for w in words:
+#     print(w, len(w))
+
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
