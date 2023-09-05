36141a8a04fe1ea09168e57508777b4f5a0c167d
Consultant <samirsamado5@gmail.com>
2023-09-05 16:58:43 -0400

    default arguments values

--- PythonProjects/tests.py
+++ PythonProjects/tests.py
@@ -49,33 +49,44 @@ users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
 #         case _:
 #             return "Something's wrong with the internet"
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+# def where_is(point):
+#     match point:
+#         case Point(x=0, y=0):
+#             print("Origin")
+#         case Point(x=0, y=y):
+#             print(f"Y={y}")
+#         case Point(x=x, y=0):
+#             print(f"X={x}")
+#         case Point():
+#             print("Somewhere else")
+#         case _:
+#             print("Not a point")
+#
+# match point:
+#     case (0, 0):
+#         print("Origin")
+#     case (0, y):
+#         print(f"Y={y}")
+#     case (x, 0):
+#         print(f"X={x}")
+#     case (x, y):
+#         print(f"X={x}, Y={y}")
+#     case _:
+#         raise ValueError("Not a point")
 
-def where_is(point):
-    match point:
-        case Point(x=0, y=0):
-            print("Origin")
-        case Point(x=0, y=y):
-            print(f"Y={y}")
-        case Point(x=x, y=0):
-            print(f"X={x}")
-        case Point():
-            print("Somewhere else")
-        case _:
-            print("Not a point")
-
-match point:
-    case (0, 0):
-        print("Origin")
-    case (0, y):
-        print(f"Y={y}")
-    case (x, 0):
-        print(f"X={x}")
-    case (x, y):
-        print(f"X={x}, Y={y}")
-    case _:
-        raise ValueError("Not a point")
-
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
