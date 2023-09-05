068084dd5c7438b84c0f18e6f0d32466ff5670ae
Consultant <samirsamado5@gmail.com>
2023-09-05 16:40:44 -0400

    for loop

--- PythonProjects/tests.py
+++ PythonProjects/tests.py
@@ -1,11 +1,15 @@
-x = int(input("Please enter an integer: "))
+# x = int(input("Please enter an integer: "))
+#
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
 
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
