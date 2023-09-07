452d837f2b759243d156eca80ff2395e591b239a
Consultant <samirsamado5@gmail.com>
2023-09-07 10:56:34 -0400

    connected successfully with mysql+pandas

--- PythonProjects/MsqlConnectorSQLAlchemy.py
+++ PythonProjects/MsqlConnectorSQLAlchemy.py
@@ -1,23 +1,3 @@
-# from sqlalchemy import create_engine, text
-#
-# # Create an engine object
-# engine = create_engine('mysql+pymysql://saber:Flex2020Flex@localhost:3306/BIGDATABASE')
-#
-# # Create a text clause object
-# query = text('SELECT * FROM Employee')
-#
-# # Execute the query
-# connection = engine.connect()
-# cursor = connection.execute(query)
-# results = cursor.fetchall()
-#
-# # Print the results
-# for row in results:
-#     print(row)
-#
-# # Close the connection
-# connection.close()
-
 import sqlalchemy
 import pandas as pd
 from sqlalchemy import text
