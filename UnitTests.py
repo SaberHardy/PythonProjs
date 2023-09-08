142cfeeca6a7793731b8c0d533689a54408ccece
Consultant <samirsamado5@gmail.com>
2023-09-08 08:32:50 -0400

    add unittest

---
+++ UnitTests.py
@@ -0,0 +1,35 @@
+import unittest
+
+# class TestSum(unittest.TestCase):
+#
+#     def test_positive_numbers(self):
+#         self.assertEqual(sum([1, 2, 3]), 6)
+#
+#     def test_negative_numbers(self):
+#         self.assertEqual(sum([-1, -2, 3]), -6)
+#
+#     def test_mixed_numbers(self):
+#         self.assertEqual(sum([1, 2, -3]), 0)
+
+
+# def reverse_string(string):
+#     reversed_string = ""
+#     for i in range(len(string) - 1, -1, -1):
+#         reversed_string += string[i]
+#     return reversed_string
+
+
+# class TestReverseString(unittest.TestCase):
+#
+#     def test_empty_string(self):
+#         self.assertEqual(reverse_string(""), "")
+#
+#     def test_single_character_string(self):
+#         self.assertEqual(reverse_string("a"), "a")
+#
+#     def test_multi_character_string(self):
+#         self.assertEqual(reverse_string("hello"), "olleh")
+
+
+if __name__ == '__main__':
+    unittest.main()
